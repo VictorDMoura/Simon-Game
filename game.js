ce33fe854ea6e98c0c849cec0981500a71c99e7a
VictorDMoura <mouramvictor@gmail.com>
2023-01-23 13:45:58 -0300

    Files

---
+++ game.js
@@ -0,0 +1,84 @@
+let buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+let userClickPattern = [];
+
+let gameStart = false;
+let level = 0;
+
+function nextSequence() {
+
+  userClickPattern = [];
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  
+  level++;
+  $("#level-title").text("level " + level)
+  
+
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+  
+}
+
+
+
+$(".btn").click(function(){
+    let userChosenColour = $(this).attr("id");
+    userClickPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickPattern.length - 1)
+
+})
+
+
+function playSound(name){
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+
+function animatePress(currentColour){
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+$(document).keypress(function(){
+  if(!gameStart){
+    nextSequence();
+    gameStart = true;
+  }
+});
+
+function checkAnswer(currentLevel){
+  if(gamePattern[currentLevel] === userClickPattern[currentLevel]){
+    console.log("sucess");
+    if(gamePattern.length ===  userClickPattern.length){
+      setTimeout(function (){
+        nextSequence();
+      },1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function (){
+      $("body").removeClass("game-over");
+    },200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $(document).keypress(startOver())
+  }
+
+}
+
+function startOver(){
+  gamePattern = [];
+  userClickPattern = [];
+  gameStart = false;
+  level = 0;
+  
+}
